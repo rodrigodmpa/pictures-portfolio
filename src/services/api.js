c43f08a5385a839234d7baf09857a58aa39b17c4
Rodrigo Demetrio Palma <rodrigo.demetrio.palma@gmail.com>
2020-12-08 16:08:58 -0300

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://3.23.213.205:3333',
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_BASE_BACKEND_URL,
+  // baseURL: 'http://3.23.213.205',
+  // baseURL: 'http://localhost:3333',
 });
 
 export default api;
